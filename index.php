4a8c20b6b336b87cec75e9ee1b6a38bb4d5802b4
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2021-12-11 16:39:32 +0900

    iconの設定

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<!--<link rel="manifest" href="manifest.json">-->
+
 <?php
 function h($str)
 {
@@ -25,10 +25,8 @@ if($data = file_get_contents(h($url))){
 ?>
 
 <link rel="manifest" href=<?php
-$img = base64_encode(file_get_contents('https://www.google.com/s2/favicons?domain_url='.$url));
-
-$manifest=base64_encode('{"short_name": "'.$title.'","name": "'.$title.'","display": "standalone","start_url": "index.html"}]}');
-echo 'data:application/json;base64,'.$manifest;
+$manifest=base64_encode('{"short_name": "'.$title.'","name": "'.$title.'","display": "standalone","start_url": "index.html","icons": [{"src": "icon.png","sizes": "192x192","type": "image/png"}]}');
+echo '"data:application/json;base64,'.$manifest.'"';
 ?>>
 <title><?php echo $title;?></title>
 <style>
